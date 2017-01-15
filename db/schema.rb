cfda9f670a32f79906ae85a0a5298e42d66607be
charvine <crossovermeister@gmail.com>
2017-01-16 00:00:56 +0800

    create seed

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,12 @@ ActiveRecord::Schema.define(version: 20170114061929) do
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.integer  "wage_upper_bound"
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
-    t.boolean  "is_hidden",        default: true
+    t.boolean  "is_hidden",        default: false
   end
 
   create_table "resumes", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+  puts "這個種子檔會自動建立一個帳號, 並且創建 10 個 jobs"
+
+create_account = User.create([email: 'admin@gmail.com', password: '111111', password_confirmation: '111111', :is_admin => true])
+
+
+  create_workers = for i in 1..5 do
+                    Job.create!([title: "打雜#{i}號", description: "打醬油的", wage_lower_bound: "#{i*1000}", wage_upper_bound: "#{i*2000}"])
+
+                  end
+Job.create!([title: "CEO", description: "當老大", wage_lower_bound: "#{50000}",
+ wage_upper_bound: "#{100000}"])
+
+ Job.create!([title: "VP", description: "當老二", wage_lower_bound: "#{30000}",
+  wage_upper_bound: "#{70000}"])
+
+  create_managers = for i in 1..3 do
+                    Job.create!([title: "第#{i}部門經理", description: "罵人的", wage_lower_bound: "#{i*10000}", wage_upper_bound: "#{i*20000}"])
+
+                  end
