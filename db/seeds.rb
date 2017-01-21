768183f5657db7bb6758468309656e7866288e4a
charvine <crossovermeister@gmail.com>
2017-01-21 22:44:26 +0800

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,17 +12,21 @@
 create_account = User.create([email: 'admin@gmail.com', password: '111111', password_confirmation: '111111', :is_admin => true])
 
 
-  create_workers = for i in 1..5 do
-                    Job.create!([title: "编程小白#{i}號", description: "累了就睡", wage_lower_bound: "#{i*1000}", wage_upper_bound: "#{i*2000}"])
+  create_workers = for i in 1..3 do
+                    Job.create!([title: "编程小白#{i}號", description: "没有一颗坚强的心", wage_lower_bound: "#{i*1000}", wage_upper_bound: "#{i*2000}"])
 
                   end
-Job.create!([title: "战神", description: "當老大", wage_lower_bound: "#{50000}",
+Job.create!([title: "战友", description: "自律、自信、自由", wage_lower_bound: "#{50000}",
  wage_upper_bound: "#{100000}"])
 
- Job.create!([title: "VP", description: "當老二", wage_lower_bound: "#{30000}",
+ Job.create!([title: "全栈人", description: "从0到1，从1到N", wage_lower_bound: "#{30000}",
   wage_upper_bound: "#{70000}"])
 
   create_managers = for i in 1..3 do
-                    Job.create!([title: "第#{i}部門經理", description: "罵人的", wage_lower_bound: "#{i*1}", wage_upper_bound: "#{i*20000}"])
+                    Job.create!([title: "Growth Hacker No.#{i}", description: "市调、行销、管理", wage_lower_bound: "#{i*1}", wage_upper_bound: "#{i*20000}"])
 
                   end
+                  create_techniques = for i in 1..3 do
+                                    Job.create!([title: "高级新手 No.#{i}", description: "市调、行销、管理", wage_lower_bound: "#{i*1}", wage_upper_bound: "#{i*20000}"])
+
+                                  end
